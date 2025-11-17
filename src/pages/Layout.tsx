2d857eee708c4527b925792d57040f5523f17c6e
viniciushds99 <viniciushds99@gmail.com>
2025-11-17 18:31:05 -0300

    Correção na scroll da responsividade.

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,7 +4,7 @@ import Background from "../assets/moon.jpg"
 
 export const Layout = () => {
   return (
-    <main className="relative min-h-screen overflow-hidden">
+    <main className="relative min-h-screen overflow-y-hidden lg:overflow-hidden">
       <div
         className="absolute inset-0 bg-cover bg-center blur-[10px]"
         style={{ backgroundImage: `url('${Background}')` }}
